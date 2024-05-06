9c62a9bd394cb4a96e5ec08cd96002c6aef7aab0
Nelson Durrant <snelsondurrant@gmail.com>
2024-05-06 16:11:39 -0400

    Updates to setup

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ sudo cp 00-teensy.rules /etc/udev/rules.d/00-teensy.rules
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-# Pull latest version of Docker repo
+# Add user permissions
 sudo usermod -aG docker frostlab
 sudo su - frostlab
-docker pull snelsondurrant/cougars:latest
