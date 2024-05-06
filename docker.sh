93181f53fef6dbd4166dd774bc190d2b85e1f68f
Nelson Durrant <snelsondurrant@gmail.com>
2024-05-06 18:29:33 -0400

    Added extra script

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker pull snelsondurrant/cougars:latest
-docker run -it --rm --user frostlab --net=host --privileged -v /dev:/dev -v /run/udev:/run/udev:ro -v /home/frostlab/bag:/home/frostlab/ros2_ws/bag snelsondurrant/cougars:latest
+docker run -it --rm --name cougars --user frostlab --net=host --privileged -v /dev:/dev -v /run/udev:/run/udev:ro -v /home/frostlab/bag:/home/frostlab/ros2_ws/bag snelsondurrant/cougars:latest
