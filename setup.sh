4e92f7e0595840945637072c8b9f66ed299c4c21
Nelson Durrant <snelsondurrant@gmail.com>
2024-05-06 14:42:23 -0400

    Testing

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # Set up udev rules
-sudo cp 00-teensy.rules /etc/udev/rules.d/00-teensy.rules
+# sudo cp 00-teensy.rules /etc/udev/rules.d/00-teensy.rules
 
 # Make local log directory
-mkdir ~/bag/
+# mkdir ~/bag/
 
 # Install Docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo sh get-docker.sh
 
 # Pull latest version of Docker repo
-sudo groupadd docker
 sudo usermod -aG docker frostlab
 su -s frostlab
 docker pull snelsondurrant/cougars:latest
