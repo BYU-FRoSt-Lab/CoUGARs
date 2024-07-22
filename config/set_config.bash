5fbafec4b6da0277a5d92b2f1ee590552e34caf4
Nelson Durrant <snelsondurrant@gmail.com>
2024-07-22 17:42:26 -0600

    Param updates

--- config/set_config.bash
+++ config/set_config.bash
@@ -30,4 +30,5 @@ source install/setup.bash
 
 ros2 topic pub --once /calibration frost_interfaces/msg/Calibration "{depth_kp: $DEPTH_KP, depth_ki: $DEPTH_KI, depth_kd: $DEPTH_KD, depth_min_output: $DEPTH_MIN_OUTPUT, depth_max_output: $DEPTH_MAX_OUTPUT, depth_bias: $DEPTH_BIAS, heading_kp: $HEADING_KP, heading_ki: $HEADING_KI, heading_kd: $HEADING_KD, heading_min_output: $HEADING_MIN_OUTPUT, heading_max_output: $HEADING_MAX_OUTPUT, heading_bias: $HEADING_BIAS, velocity_kp: $VELOCITY_KP, velocity_ki: $VELOCITY_KI, velocity_kd: $VELOCITY_KD, velocity_min_output: $VELOCITY_MIN_OUTPUT, velocity_max_output: $VELOCITY_MAX_OUTPUT, velocity_bias: $VELOCITY_BIAS}"
 
-# TODO: Add ROS params as necessary, maybe vehicle ID?
+# Add more ROS params here as needed
+ros2 param set /controller vehicle_id 0
