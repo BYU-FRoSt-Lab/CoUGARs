19fa4aba4a598f0fccea437a4d9050132e5224b4
bradenMeyers <braden@themeyers.net>
2024-07-25 15:30:03 -0600

    Notes on how to find Teensy ID

--- config/upload_control_helper.bash
+++ config/upload_control_helper.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#To find CONTROL_ID plug in the teensy that will run the control to the RPI and unplug the other teensy
+#Run tycmd list and copy the ID (ie. add 15569680-Teensy Teensy 4.1, you would copy 15569680)
+
 CONTROL_ID="ADD HERE"
 
 cd ~/teensy_ws/control/.pio/build/teensy41
--- config/upload_sensors_helper.bash
+++ config/upload_sensors_helper.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#To find SENSORS_ID plug in the teensy that will run the control to the RPI and unplug the other teensy
+#Run tycmd list and copy the ID (ie. add 15569680-Teensy Teensy 4.1, you would copy 15569680)
+
 SENSORS_ID="ADD HERE"
 
 cd ~/teensy_ws/sensors/.pio/build/teensy41
