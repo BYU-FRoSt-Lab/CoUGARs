eb24b140d58d428a670774cb2ef12ecd205cdfd6
frostlab <frostlab.emails@gmail.com>
2024-07-25 21:09:47 +0000

    coug1 teensy id

--- config/upload_control_helper.bash
+++ config/upload_control_helper.bash
@@ -3,7 +3,7 @@
 #To find CONTROL_ID plug in the teensy that will run the control to the RPI and unplug the other teensy
 #Run tycmd list and copy the ID (ie. add 15569680-Teensy Teensy 4.1, you would copy 15569680)
 
-CONTROL_ID="ADD HERE"
+CONTROL_ID="15569680"
 
 cd ~/teensy_ws/control/.pio/build/teensy41
 tycmd upload --board $CONTROL_ID firmware.hex
--- config/upload_sensors_helper.bash
+++ config/upload_sensors_helper.bash
@@ -3,7 +3,7 @@
 #To find SENSORS_ID plug in the teensy that will run the control to the RPI and unplug the other teensy
 #Run tycmd list and copy the ID (ie. add 15569680-Teensy Teensy 4.1, you would copy 15569680)
 
-SENSORS_ID="ADD HERE"
+SENSORS_ID="14484980"
 
 cd ~/teensy_ws/sensors/.pio/build/teensy41
 tycmd upload --board $SENSORS_ID firmware.hex
