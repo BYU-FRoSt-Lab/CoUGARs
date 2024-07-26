7d50d0028842e7b2d6ac7dd29d38e7a282906d79
Nelson Durrant <snelsondurrant@gmail.com>
2024-07-26 16:02:41 -0600

    Updates to naming conventions

--- config/set_config.sh
+++ config/set_config.sh
@@ -34,6 +34,6 @@ VELOCITY_BIAS=0
 cd ~/ros2_ws
 source install/setup.bash
 
-ros2 topic pub --once /calibration frost_interfaces/msg/Calibration "{depth_kp: $DEPTH_KP, depth_ki: $DEPTH_KI, depth_kd: $DEPTH_KD, depth_min_output: $DEPTH_MIN_OUTPUT, depth_max_output: $DEPTH_MAX_OUTPUT, depth_bias: $DEPTH_BIAS, heading_kp: $HEADING_KP, heading_ki: $HEADING_KI, heading_kd: $HEADING_KD, heading_min_output: $HEADING_MIN_OUTPUT, heading_max_output: $HEADING_MAX_OUTPUT, heading_bias: $HEADING_BIAS, velocity_kp: $VELOCITY_KP, velocity_ki: $VELOCITY_KI, velocity_kd: $VELOCITY_KD, velocity_min_output: $VELOCITY_MIN_OUTPUT, velocity_max_output: $VELOCITY_MAX_OUTPUT, velocity_bias: $VELOCITY_BIAS}"
+ros2 topic pub --once /control_config frost_interfaces/msg/ControlConfig "{depth_kp: $DEPTH_KP, depth_ki: $DEPTH_KI, depth_kd: $DEPTH_KD, depth_min_output: $DEPTH_MIN_OUTPUT, depth_max_output: $DEPTH_MAX_OUTPUT, depth_bias: $DEPTH_BIAS, heading_kp: $HEADING_KP, heading_ki: $HEADING_KI, heading_kd: $HEADING_KD, heading_min_output: $HEADING_MIN_OUTPUT, heading_max_output: $HEADING_MAX_OUTPUT, heading_bias: $HEADING_BIAS, velocity_kp: $VELOCITY_KP, velocity_ki: $VELOCITY_KI, velocity_kd: $VELOCITY_KD, velocity_min_output: $VELOCITY_MIN_OUTPUT, velocity_max_output: $VELOCITY_MAX_OUTPUT, velocity_bias: $VELOCITY_BIAS}"
 
-ros2 param set /controller vehicle_id $VEHICLE_ID
+ros2 param set /moos_bridge vehicle_id $VEHICLE_ID
