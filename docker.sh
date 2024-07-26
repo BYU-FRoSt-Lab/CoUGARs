05f23be1e20a1f9f5112b98943f40f795bf5cbe6
bradenMeyers <braden@themeyers.net>
2024-07-26 10:31:44 -0600

    fixed bash naming

--- docker.sh
+++ docker.sh
@@ -7,5 +7,6 @@
 #   it's still running if you're working on image dev
 ##########################################################
 
-docker pull snelsondurrant/cougars:latest
-docker run -it --rm --name cougars --user frostlab --net=host --privileged -v /home/frostlab/bag:/home/frostlab/ros2_ws/bag:rw -v /home/frostlab/config:/home/frostlab/config:rw -v /dev:/dev:rw -v /run/udev:/run/udev:ro snelsondurrant/cougars:latest
+# docker pull snelsondurrant/cougars:latest
+# docker run -it --rm --name cougars --user frostlab --net=host --privileged -v /home/frostlab/bag:/home/frostlab/ros2_ws/bag:rw -v /home/frostlab/config:/home/frostlab/config:rw -v /dev:/dev:rw -v /run/udev:/run/udev:ro snelsondurrant/cougars:latest
+docker compose up -d
--- setup_tycmd.bash
+++
@@ -1,7 +0,0 @@
-sudo mkdir -p -m0755 /etc/apt/keyrings
-
-sudo curl https://download.koromix.dev/debian/koromix-archive-keyring.gpg -o /etc/apt/keyrings/koromix-archive-keyring.gpg
-sudo echo "deb [signed-by=/etc/apt/keyrings/koromix-archive-keyring.gpg] https://download.koromix.dev/debian stable main" > /etc/apt/sources.list.d/koromix.dev-stable.list
-
-sudo apt update
-sudo apt install tytools
