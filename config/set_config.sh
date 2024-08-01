9075fea15af94fb4fa51407b03042f74933eb135
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-01 13:09:25 -0600

    naming updates

--- config/set_config.sh
+++ config/set_config.sh
@@ -37,3 +37,4 @@ source install/setup.bash
 ros2 topic pub --once /control_config frost_interfaces/msg/ControlConfig "{depth_kp: $DEPTH_KP, depth_ki: $DEPTH_KI, depth_kd: $DEPTH_KD, depth_min_output: $DEPTH_MIN_OUTPUT, depth_max_output: $DEPTH_MAX_OUTPUT, depth_bias: $DEPTH_BIAS, heading_kp: $HEADING_KP, heading_ki: $HEADING_KI, heading_kd: $HEADING_KD, heading_min_output: $HEADING_MIN_OUTPUT, heading_max_output: $HEADING_MAX_OUTPUT, heading_bias: $HEADING_BIAS, velocity_kp: $VELOCITY_KP, velocity_ki: $VELOCITY_KI, velocity_kd: $VELOCITY_KD, velocity_min_output: $VELOCITY_MIN_OUTPUT, velocity_max_output: $VELOCITY_MAX_OUTPUT, velocity_bias: $VELOCITY_BIAS}"
 
 ros2 param set /moos_bridge vehicle_id $VEHICLE_ID
+ros2 param set /manual_control vehicle_id $VEHICLE_ID
