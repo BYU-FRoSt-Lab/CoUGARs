3e6c4b8d9b2447f19958c35a3f0610d8fea343b6
bradenMeyers <braden@themeyers.net>
2024-08-01 08:50:05 -0600

    more udev rules for teensys

--- setup.sh
+++ setup.sh
@@ -7,8 +7,15 @@
 #   the most current image
 ##########################################################
 
+echo "Make sure to add the serial numbers of the teensys to the 99-teensy.rules file"
+echo "Enter the same serial numbers into the upload helper bash scripts in the config folder"
+echo "You can find the serial numbers by running: tycmd list"
 # Set up udev rules
 sudo cp 00-teensy.rules /etc/udev/rules.d/00-teensy.rules
+sudo cp 99-teensy.rules /etc/udev/rules.d/99-teensy.rules
+
+sudo udevadm control --reload-rules
+sudo udevadm trigger
 
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
