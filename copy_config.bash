7ca17bc38a8e1f6f2bd9d2c93b24c4b0e98a7a41
bradenMeyers <braden@themeyers.net>
2024-08-08 18:01:32 -0600

    once more

--- copy_config.bash
+++ copy_config.bash
@@ -4,7 +4,7 @@
 cp -r /home/frostlab/CougarsSetup/config /home/frostlab/config
 
 # Set up udev rules
-cd ~/config
+cd /home/frostlab/config
 bash set_rules.sh
 
 echo "ALERT: Make sure to set the vehicle-specific params in "set_config.sh," "control_id.sh," "sensors_id.sh," and "set_rules.sh" in "~/config" now"
