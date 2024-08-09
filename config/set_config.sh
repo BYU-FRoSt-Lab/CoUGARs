bf945ea7edb55de9f2442e9b142ed7046a8c1f51
bradenMeyers <braden@themeyers.net>
2024-08-08 18:03:58 -0600

    no root directory

--- config/set_config.sh
+++ config/set_config.sh
@@ -31,7 +31,7 @@ VELOCITY_MIN_OUTPUT=0
 VELOCITY_MAX_OUTPUT=0
 VELOCITY_BIAS=0
 
-cd ~/ros2_ws
+cd /home/frostlab/ros2_ws
 source install/setup.bash
 
 ros2 topic pub --once /control_config frost_interfaces/msg/ControlConfig "{depth_kp: $DEPTH_KP, depth_ki: $DEPTH_KI, depth_kd: $DEPTH_KD, depth_min_output: $DEPTH_MIN_OUTPUT, depth_max_output: $DEPTH_MAX_OUTPUT, depth_bias: $DEPTH_BIAS, heading_kp: $HEADING_KP, heading_ki: $HEADING_KI, heading_kd: $HEADING_KD, heading_min_output: $HEADING_MIN_OUTPUT, heading_max_output: $HEADING_MAX_OUTPUT, heading_bias: $HEADING_BIAS, velocity_kp: $VELOCITY_KP, velocity_ki: $VELOCITY_KI, velocity_kd: $VELOCITY_KD, velocity_min_output: $VELOCITY_MIN_OUTPUT, velocity_max_output: $VELOCITY_MAX_OUTPUT, velocity_bias: $VELOCITY_BIAS}"
--- config/set_rules.sh
+++ config/set_rules.sh
@@ -6,8 +6,8 @@
 #   "99-teensy.rules" file
 ##########################################################
 
-sudo cp ~/config/00-teensy.rules /etc/udev/rules.d/00-teensy.rules
-sudo cp ~/config/99-teensy.rules /etc/udev/rules.d/99-teensy.rules
+sudo cp /home/frostlab/config/00-teensy.rules /etc/udev/rules.d/00-teensy.rules
+sudo cp /home/frostlab/config/99-teensy.rules /etc/udev/rules.d/99-teensy.rules
 
 sudo udevadm control --reload-rules
 sudo udevadm trigger
