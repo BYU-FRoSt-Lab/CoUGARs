69ec7c4b97b95e89b6f91e1f747e77dc24a8f02f
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-12 13:55:44 -0600

    Updates to control node, simplifying

--- setup.sh
+++ setup.sh
@@ -11,6 +11,9 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
+# Add user permissions
+sudo usermod -aG docker frostlab
+
 # Set up volumes
 mkdir ~/bag
 cp -r ~/CougarsSetup/config ~/config
@@ -20,6 +23,3 @@ cd ~/config
 bash set_rules.sh
 
 echo "ALERT: Make sure to set the vehicle-specific params in "set_config.sh," "control_id.sh," "sensors_id.sh," and "set_rules.sh" in "~/config" now"
-
-# Add user permissions
-sudo usermod -aG docker frostlab
