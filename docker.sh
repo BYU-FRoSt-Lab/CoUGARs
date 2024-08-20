cba9707ccb7b64f643fdc16e5eeaa63c6b302449
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-20 12:35:26 -0600

    Documentation updates

--- docker.sh
+++ docker.sh
@@ -4,6 +4,8 @@
 # PULLS AND RUNS THE NEWEST DOCKER IMAGE
 # - Run this script after running "setup.sh" to pull the
 #   most recent image and run it
+# - This can also be used to open a new bash terminal in
+#   an already running container
 ##########################################################
 
 docker pull snelsondurrant/cougars:latest
