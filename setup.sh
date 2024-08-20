80fb7c0eed28906286b2f9d6bad7250412300bf3
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-20 16:03:38 -0600

    Removed dialout

--- setup.sh
+++ setup.sh
@@ -13,7 +13,6 @@ sudo sh get-docker.sh
 
 # Add user permissions
 sudo usermod -aG docker frostlab
-sudo usermod -aG dialout frostlab
 
 # Set up udev rules
 sudo cp /home/frostlab/CougarsSetup/00-teensy.rules /etc/udev/rules.d/00-teensy.rules
