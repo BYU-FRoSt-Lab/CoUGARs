2c3f6813b53b3d0055c12e8763d54b217a671efc
Matthew McMurray <mmcmurray123@gmail.com>
2024-08-29 12:49:35 -0600

    first

--- config/rpi_id.sh
+++ config/rpi_id.sh
@@ -5,6 +5,6 @@
 ##########################################################
 
 # run (ifconfig eth0 | grep 'inet ' | awk '{print $2}'), and then put that number 
-
+# for 1 it is 192.168.194.59
 STATIC_IP=192.168.194.59
 
