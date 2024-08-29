f443b4895297665fd0874ecd6cfe633324ac3348
Matthew McMurray <mmcmurray123@gmail.com>
2024-08-29 12:41:03 -0600

    first add

---
+++ config/rpi_id.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##########################################################
+# static ip address of Pi
+##########################################################
+
+# run (ifconfig eth0 | grep 'inet ' | awk '{print $2}'), and then put that number 
+
+STATIC_IP=192.168.194.59
+
