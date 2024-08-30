78f4af4902cbbb6626a4623ecd781d9e18165d95
Nelson Durrant <snelsondurrant@gmail.com>
2024-08-30 10:14:06 -0600

    Updates to compose

--- compose.sh
+++ compose.sh
@@ -2,7 +2,7 @@
 
 ##########################################################
 # PULLS AND RUNS THE NEWEST DOCKER IMAGE
-# - Specify the dev image by running "bash compose.sh dev"
+# - Use "bash compose.sh down" to stop the image
 # - Run this script after running "setup.sh" to pull the
 #   most recent image and run it
 # - This can also be used to open a new bash terminal in
@@ -11,21 +11,39 @@
 
 cd ~/CougarsSetup
 case $1 in
-    dev)
-        echo ""
-        echo "ALERT: Loading the development image (amd64)..."
-        echo ""
+    down)
 
-        docker compose -f docker/docker-compose-amd64.yaml up -d
+        # check the system architecture
+        if [ "$(uname -m)" == "aarch64" ]; then
+            echo ""
+            echo "ALERT: Stopping the vehicle image..."
+            echo ""
+
+            docker compose -f docker/docker-compose-arm64.yaml down
+        else
+            echo ""
+            echo "ALERT: Stopping the development image..."
+            echo ""
+
+            docker compose -f docker/docker-compose-amd64.yaml down
+        fi
         ;;
     *)
-        echo ""
-        echo "ALERT: Loading the latest vehicle image (arm64)..."
-        echo "Load the development image (amd64) by running 'bash compose.sh dev'"
-        echo ""
+        
+        # check the system architecture
+        if [ "$(uname -m)" == "aarch64" ]; then
+            echo ""
+            echo "ALERT: Loading the vehicle image (arm64)..."
+            echo ""
 
-        docker compose -f docker/docker-compose-arm64.yaml up -d
+            docker compose -f docker/docker-compose-arm64.yaml up -d
+        else
+            echo ""
+            echo "ALERT: Loading the development image (amd64)..."
+            echo ""
+
+            docker compose -f docker/docker-compose-amd64.yaml up -d
+        fi
+        docker exec -it cougars bash
         ;;
 esac
-
-docker exec -it cougars bash
