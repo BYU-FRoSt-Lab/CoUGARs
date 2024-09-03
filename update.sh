cb0aba5dcb6043575818c55ade482232f402490f
Nelson Durrant <snelsondurrant@gmail.com>
2024-09-03 14:18:20 -0600

    Added update script

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##########################################################
+# PULLS ALL AVAILABLE GIT UPDATES
+##########################################################
+
+cd ~/CougarsSetup
+git pull
+
+cd ~/CougarsRPi
+git pull
+
+cd ~/CougarsTeensy
+git pull
