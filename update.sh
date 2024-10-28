13370908a788cf7293cc65d139fe69a61213f618
Nelson Durrant <snelsondurrant@gmail.com>
2024-10-28 11:41:29 -0600

    Added docker pull to update

--- update.sh
+++ update.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Created by Nelson Durrant, Sep 2024
 #
-# Simply pulls all avaliable git updates
+# Simply pulls all avaliable updates
 # - Make sure you run this from the root of the top-level repo
 
+# Docker updates
+docker pull frostlab/cougars:latest
+
+# GitHub updates
 git pull
 
 cd ./cougars-ros2
