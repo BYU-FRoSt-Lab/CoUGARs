328f81213e92d3db58dac8a781bca76477dc638f
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-08 14:19:06 -0700

    Documentation updates

--- templates/bash_params.sh
+++ templates/bash_params.sh
@@ -14,7 +14,7 @@ export LABNAME=frostlab # ex. frostlab
 # IMPORTANT!: Match with the namespace defined in 'teensy_ws/cougars/include/namespace.h'
 # You need to update that namespace in the Teensy code as well
 export NAMESPACE=coug0 # ex. coug0
-export VEHICLE_PARAM_FILE=/home/$LABNAME/config/vehicle_params.yaml # ex. /home/$LABNAME/config/vehicle_params.yaml
+export VEHICLE_PARAMS_FILE=/home/$LABNAME/config/vehicle_params.yaml # ex. /home/$LABNAME/config/vehicle_params.yaml
 
 # To find the STATIC_IP, run "ifconfig eth0 | grep 'inet ' | awk '{print $2}'"
 # from OUTSIDE THE DOCKER CONTAINER and copy that number (ex. "192.168.194.59")
