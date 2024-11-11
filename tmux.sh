3bd4f73dbe1619c5d458c220720432dbc29085ef
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-11 14:42:43 -0700

    Bash updates

--- tmux.sh
+++ tmux.sh
@@ -5,15 +5,15 @@
 # - Use 'bash tmux.sh kill' to kill the session
 
 function printInfo {
-  echo -e "\033[0m\033[36m[INFO] $1\033[0m"
+  echo -e "\033[0m\033[96m[INFO] $1\033[0m"
 }
 
 function printWarning {
-  echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
+  echo -e "\033[0m\033[93m[WARNING] $1\033[0m"
 }
 
 function printError {
-  echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
+  echo -e "\033[0m\033[91m[ERROR] $1\033[0m"
 }
 
 case $1 in
