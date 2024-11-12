b3eb3b040262152136d8bfb3235531f8388bc51e
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-11 17:25:17 -0700

    Coloring fixes

--- setup.sh
+++ setup.sh
@@ -10,15 +10,15 @@
 # - Make sure you run this from the root of the top-level repo
 
 function printInfo {
-  echo -e "\033[0m\033[96m[INFO] $1\033[0m"
+  echo -e "\033[0m\033[36m[INFO] $1\033[0m"
 }
 
 function printWarning {
-  echo -e "\033[0m\033[93m[WARNING] $1\033[0m"
+  echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
 }
 
 function printError {
-  echo -e "\033[0m\033[91m[ERROR] $1\033[0m"
+  echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
 }
 
 if [ "$(uname -m)" == "aarch64" ]; then
