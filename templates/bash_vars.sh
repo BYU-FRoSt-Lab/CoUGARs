9ffa3b57ec2a83ffb127a502cc24f07f9832de31
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-12 10:06:10 -0700

    Updates to gpio chip

--- templates/bash_vars.sh
+++ templates/bash_vars.sh
@@ -8,13 +8,12 @@
 # - If you edit this file, you'll need to make sure the 
 #   copied file in 'config' is updated as well
 
-# IMPORTANT!: Match with the lab name defined in the Dockerfile
-export LABNAME=frostlab # ex. frostlab
-
 # IMPORTANT!: Match with the namespace defined in 'teensy_ws/cougars/include/namespace.h'
-# You need to update that namespace in the Teensy code as well
 export NAMESPACE=coug0 # ex. coug0
-export VEHICLE_PARAMS_FILE=/home/$LABNAME/config/vehicle_params.yaml # ex. /home/$LABNAME/config/vehicle_params.yaml
+export VEHICLE_PARAMS_FILE=/home/frostlab/config/vehicle_params.yaml # ex. /home/$LABNAME/config/vehicle_params.yaml
+
+# IMPORTANT!: Match with the path defined in 'gpio/gpio_tools/gpio_def.py'
+export GPIO_CHIP=/dev/gpiochip4 # ex. /dev/gpiochip4
 
 # To find the STATIC_IP, run "ifconfig eth0 | grep 'inet ' | awk '{print $2}'"
 # from OUTSIDE THE DOCKER CONTAINER and copy that number (ex. "192.168.194.59")
