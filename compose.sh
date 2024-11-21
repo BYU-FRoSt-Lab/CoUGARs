d03fa19e2040116b67df0b07602a35ea0b93c2be
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-20 18:11:26 -0700

    Code cleanup

--- compose.sh
+++ compose.sh
@@ -43,8 +43,10 @@ case $1 in
       printInfo "Loading the development image..."
       docker compose -f docker/docker-compose-dev.yaml up -d
     fi
+
     # Quick permission fix for GPIO access in the container
     docker exec --user root cougars bash -c "bash /home/frostlab/gpio/permission_fix.sh $GPIO_CHIP"
+    
     docker exec -it cougars bash
   ;;
 esac
