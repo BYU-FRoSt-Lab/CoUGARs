daa61417601b07dd37fc204f72d13090b49c2562
Brighton Anderson <bja1701@byu.edu>
2024-11-21 09:17:25 -0700

    added pAntler timeout

--- tmux.sh
+++ tmux.sh
@@ -27,14 +27,14 @@ case $1 in
 
       printInfo "Starting the tmux session..."
 
+      ### FIRST WINDOW - ROS SCRIPTS ###
+
       # Start the tmux session
       tmux new-session -d -s cougars
       tmux split-window -h -t cougars
       tmux split-window -v -t cougars
       tmux select-pane -t cougars:0.0
       tmux split-window -v -t cougars
-      tmux select-pane -t cougars:0.2
-      tmux split-window -h -t cougars
       tmux select-pane -t cougars:0.0
 
       # Send commands to the tmux session
@@ -46,42 +46,32 @@ case $1 in
       tmux send-keys -t cougars:0.2 "clear" ENTER
       tmux send-keys -t cougars:0.3 "bash compose.sh" ENTER
       tmux send-keys -t cougars:0.3 "clear" ENTER
-      # tmux send-keys -t cougars:0.4 "bash compose.sh" ENTER
-      # tmux send-keys -t cougars:0.4 "clear" ENTER
-      
 
-      
-
-      # 
-      tmux send-keys -t cougars:0.0 "cd ~/gpio" ENTER
-      tmux send-keys -t cougars:0.0 "bash permission_fix.sh" ENTER
-      # tmux send-keys -t cougars:0.0 "cd ~/ros2_ws/dvl_tools" ENTER
-      # tmux send-keys -t cougars:0.0 "bash calibrate_gyro.sh" ENTER
-      # tmux send-keys -t cougars:0.0 "cd ~/ros2_ws" ENTER
-      # tmux send-keys -t cougars:0.0 "colcon build" ENTER
-      tmux send-keys -t cougars:0.0 "sudo systemctl restart chrony" ENTER #correct spot to put this?
+      tmux send-keys -t cougars:0.0 "sudo systemctl restart chrony" ENTER 
       tmux send-keys -t cougars:0.0 "date" ENTER
-      tmux send-keys -t cougars:0.0 "ls" ENTER
-      tmux send-keys -t cougars:0.0 "bash launch.sh <put param here>" # Don't start just yet
+      tmux send-keys -t cougars:0.0 "bash launch.sh <mission_type>" # Don't start just yet
 
       tmux send-keys -t cougars:0.1 "cd ~/ros2_ws" ENTER
-      # tmux send-keys -t cougars:0.1 "colcon build" ENTER
-      tmux send-keys -t cougars:0.1 "bash test.sh <put param here>" # Don't start just yet
+      tmux send-keys -t cougars:0.1 "bash test.sh <acoustics>" # Don't start just yet
 
       tmux send-keys -t cougars:0.2 "cd ~/ros2_ws" ENTER
-      # tmux send-keys -t cougars:0.2 "colcon build" ENTER
-      tmux send-keys -t cougars:0.2 "bash record.sh" # Don't start just yet
+      tmux send-keys -t cougars:0.2 "bash record.sh <acoustics>" # Don't start just yet
 
       tmux send-keys -t cougars:0.3 "cd ~/config" ENTER
       tmux send-keys -t cougars:0.3 "cat vehicle_params.yaml" ENTER
 
-      # tmux send-keys -t cougars:0.4 "cd ~/ros2_ws/moos_tools" ENTER
-      # tmux send-keys -t cougars:0.4 "bash mission_start.sh" # Don't start just yet
+      ### SECOND WINDOW - MOOS SCRIPTS ###
+
       tmux new-window -t cougars -n moos 'bash ~/ros2_ws/moos_tools/mission_start.sh' # New window or split terminal again?
       tmux split-window -v -t moos
       tmux select-pane -t moos:0.1
+      tmux split-window -h -t moos
       tmux send-keys -t moos:0.1 "bash ~/ros2_ws/moos_tools/mission_deploy.sh" ENTER
       tmux send-keys -t moos:0.1 "cat ~/ros2_ws/moos_tools/coug.bhv" ENTER
+      tmux send-keys -t moos:0.2 "pAntler timeout 5 <command>" # Don't start just yet
+
+      # TODO: Add more terminals, etc
+      # I bet Matthew has some good ideas
 
 
     else
@@ -93,9 +83,10 @@ case $1 in
     ;;
 esac
 
+# I added some comments -Nelson
 
-#TODO: - figure out how to edit vehicle_params and moos.bhv for the mission
-#      - add in the mission_deploy.sh the pAntler timeout command
+#TODO: - figure out how to edit vehicle_params and moos.bhv for the mission (I think just cat the file so we can see it in the terminal, and we can change it using vim if needed)
+#      - add in the mission_deploy.sh the pAntler timeout command ('timeout 5 <command>' should work -- number is in seconds)
 #      - check with Braden, and Nelson to see if everything else is good to go
 #      - how to implement plot juggler
 #      - see if any other commands need to be added to the tmux script from new map waypoint stuff
